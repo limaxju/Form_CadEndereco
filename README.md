70102f0b6fa85344b823cab8dd9d28caec7a4500
julia lima <eduarda.julia@alunoescolassociais.org.br>
2023-10-04 10:14:09 -0300

    começo do preenchimento do readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Form_CadEndereco
+# Indice
+[CadEndereço](#cadastro-endereço)  
+[Descrição](#descrição)  
+[Introduçao](#introdução)  
+[Funcionalidade](#funcionalidade)  
+[Tecnologias Utilizadas](#tecnologias-utilizadas)  
+[Fontes Consulta](#fontes-consulta)  
+[Autores](#autores)  
+
+# Cadastro de endereço
+![Capa do projeto](imgcadastro.jpeg)
+## Descrição
+o projeto formulário de cadastro de endereço tem como funcionalidade demonstrar a utilização da linguagem de JavaScript para o consumo de API de endereço que esta disponível no site ViaCEP.
+## Introdução
+
+## Funcionalidade
+
+## Tecnologias Utilizadas
+* ``HTML5``
+* ``CSS3``
+* ``VSCODE``
+* ``BOOTSTRAP``
+* ``GITHUB``
+* ``GIT``
+
+## Fontes Consulta
+* [ViaCep](http://viacep.com.br/ws/$%7Bcep.value%7D/json/`)
+
+* [GitHub](https://leonardossrocha.github.io/projeto-CadEndereco/)
+
+* [BootStrap](https://getbootstrap.com/docs/5.0/forms/layout/#gutters)
+## Autores
+Julia Eduarda de Lima Barbosa
--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-'use restrict'; //modo restrito
-//https://viacep.com.br/
-
-//limpar formulário 
-//arrow function
+'use strict'; //Modo restrito
+// https://viacep.com.br/
+//função para limpar formulario
+//Arrow Function
 const limparFormulario = () =>{
     document.getElementById('rua').value = '';
-    document.getElementById('bairro').value ='';
+    document.getElementById('bairro').value = '';
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
+
 }
-//verifica se CEP é válido
-const eNumero = (numero) => /[0-9]+$/.test(numero);
+//Verifica se o CEP é válido
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+//Responsavel pelo preenchimento do formulario
+const preencherForumulario = (endereco) => {
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+
+}
+//Funcao para consumo de API da via CEP
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados = await fetch(url); // esperar
+        const address = await dados.json(); //retorna dados no formato JSON
+        if(address.hasOwnProperty('erro')){
+            alert('CEP não encontrado');
+        }else{
+            preencherForumulario( address );
+        }
+    }else{
+
+        alert('CEP incorreto');
+    }
+}
